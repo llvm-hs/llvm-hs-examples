65762879ac30fd72c94230ca24c395fdf3093388
Stephen Diehl <stephen.m.diehl@gmail.com>
2018-02-23 11:37:35 +0000

    Example of conditional branch

--- irbuilder/Main.hs
+++ irbuilder/Main.hs
@@ -9,6 +9,7 @@ import LLVM.AST hiding (function)
 import LLVM.AST.Type as AST
 import qualified LLVM.AST.Float as F
 import qualified LLVM.AST.Constant as C
+import qualified LLVM.AST.IntegerPredicate as P
 
 import LLVM.IRBuilder.Module
 import LLVM.IRBuilder.Monad
@@ -16,12 +17,19 @@ import LLVM.IRBuilder.Instruction
 
 simple :: Module
 simple = buildModule "exampleModule" $ mdo
-
-  function "add" [(i32, "a"), (i32, "b")] i32 $ \[a, b] -> mdo
-
-    entry <- block `named` "entry"; do
-      c <- add a b
-      ret c
+  function "f" [(AST.i32, "a")] AST.i32 $ \[a] -> mdo
+    entry <- block `named` "entry"
+    cond <- icmp P.EQ a (ConstantOperand (C.Int 32 0))
+    condBr cond ifThen ifElse
+    ifThen <- block
+    trVal <- add a (ConstantOperand (C.Int 32 0))
+    br ifExit
+    ifElse <- block `named` "if.else"
+    flVal <- add a (ConstantOperand (C.Int 32 0))
+    br ifElse
+    ifExit <- block `named` "if.exit"
+    r <- phi [(trVal, ifThen), (flVal, ifElse)]
+    ret r
 
 main :: IO ()
 main = print simple
